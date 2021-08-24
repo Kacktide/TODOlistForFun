bb10c5fd1405f7e3df1f38b0a8917db7a12cdf47
FURY69 <70317531+Kacktide@users.noreply.github.com>
2021-08-24 08:07:15 +0545

    Update todo.js

--- todo.js
+++ todo.js
@@ -8,14 +8,21 @@ function todoCreate() {
     `<span class="delete" onclick="this.parentElement.style.display = 'none';"><ion-icon name="trash-outline"></ion-icon></span>`;
   console.log(e);
 
-
   if (todoVal.value == "" || todoVal.value == " ") {
-        return;
-  }  else {
+    return;
+  } else {
     let todoArea = document.getElementById("eee");
     todoArea.appendChild(e);
     todoVal.value = "";
   }
-
 }
 
+const mainBtn = document.querySelectorAll("#main-btn")[0];
+
+function disable() {
+  if (!todoVal.value == "" || todoVal.value == " " || todoVal.value == "  ") {
+    mainBtn.classList.remove("disabled");
+  } else{
+    mainBtn.classList.add('disabled');
+  }
+}
